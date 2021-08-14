7d38924dbdc842a7c7843bc06bf9c695ffc4da96
Rinsvent <rinsvent007@gmail.com>
2021-08-14 17:27:59 +0700

    Отладка библиотек

--- src/DependencyInjection/RequestExtension.php
+++ src/DependencyInjection/RequestExtension.php
@@ -2,7 +2,6 @@
 
 namespace Rinsvent\RequestBundle\DependencyInjection;
 
-use Rinsvent\RequestBundle\Service\Transformer\AbstractTransformer;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
