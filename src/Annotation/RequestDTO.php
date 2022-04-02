8ca574eb461049252c1a33219da076f6ae9e2968
Rinsvent <rinsvent007@gmail.com>
2022-04-02 15:16:52 +0700

    Поправил описание атрибута

--- src/Annotation/RequestDTO.php
+++ src/Annotation/RequestDTO.php
@@ -2,7 +2,7 @@
 
 namespace Rinsvent\RequestBundle\Annotation;
 
-#[\Attribute(\Attribute::IS_REPEATABLE)]
+#[\Attribute(\Attribute::IS_REPEATABLE|\Attribute::TARGET_ALL)]
 class RequestDTO
 {
     public function __construct(
